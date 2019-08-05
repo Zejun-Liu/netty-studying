ef3f24b4f9eed5f1bab4194ea0eb1cd2e1d0ddb4
Zejun-Liu <857591294@qq.com>
2019-08-05 16:18:27 +0800

    netty

--- lesson03/src/main/java/com/jiuxian/netty/NettyServer.java
+++ lesson03/src/main/java/com/jiuxian/netty/NettyServer.java
@@ -29,33 +29,33 @@ public class NettyServer {
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
             ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel channel) {
-                            ChannelPipeline pipeline = channel.pipeline();
-                            pipeline.addLast(new HttpServerCodec());
-                            // 对写大数据流的支持
-                            pipeline.addLast(new ChunkedWriteHandler());
-                            // 对httpMessage进行聚合，聚合成FullHttpRequest或FullHttpResponse
-                            // 几乎在netty中的编程，都会使用到此handler
-                            pipeline.addLast(new HttpObjectAggregator(1024 * 64));
+            b.group(bossGroup, workerGroup);
+            b.channel(NioServerSocketChannel.class);
+            b.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                public void initChannel(SocketChannel channel) {
+                    ChannelPipeline pipeline = channel.pipeline();
+                    pipeline.addLast(new HttpServerCodec());
+                    // 对写大数据流的支持
+                    pipeline.addLast(new ChunkedWriteHandler());
+                    // 对httpMessage进行聚合，聚合成FullHttpRequest或FullHttpResponse
+                    // 几乎在netty中的编程，都会使用到此handler
+                    pipeline.addLast(new HttpObjectAggregator(1024 * 64));
 
-                            // ====================== 以上是用于支持http协议    ======================
+                    // ====================== 以上是用于支持http协议    ======================
 
-                            // ====================== 以下是支持httpWebsocket ======================
+                    // ====================== 以下是支持httpWebsocket ======================
 
-                            /*
-                             * websocket 服务器处理的协议，用于指定给客户端连接访问的路由 : /ws
-                             * 本handler会帮你处理一些繁重的复杂的事
-                             * 会帮你处理握手动作： handshaking（close, ping, pong） ping + pong = 心跳
-                             * 对于websocket来讲，都是以frames进行传输的，不同的数据类型对应的frames也不同
-                             */
-                            pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
-                            pipeline.addLast(new CustomerHandler());
-                        }
-                    });
+                    /*
+                     * websocket 服务器处理的协议，用于指定给客户端连接访问的路由 : /ws
+                     * 本handler会帮你处理一些繁重的复杂的事
+                     * 会帮你处理握手动作： handshaking（close, ping, pong） ping + pong = 心跳
+                     * 对于websocket来讲，都是以frames进行传输的，不同的数据类型对应的frames也不同
+                     */
+                    pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+                    pipeline.addLast(new CustomerHandler());
+                }
+            });
             ChannelFuture f = b.bind(8888).sync();
             f.channel().closeFuture().sync();
         } finally {
